13865e24163f3df92b0ef94cb73cc3c6ca195ab2
Reformality <zwu225@gmail.com>
2023-04-09 18:46:09 -0400

    pj4 CC.java LockTable inner Class V1.0

--- p4/src/main/java/CC/CC.java
+++ p4/src/main/java/CC/CC.java
@@ -1,8 +1,75 @@
 package CC;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class CC
 {
+	/**
+	 * Inner class representing the LockTable.
+	 */
+	private static class LockTable {
+		private Map<Integer, Map<Integer, LockType>> table;    // <RecordId, <>>
+
+		public LockTable(int recordCount) {    // new LockTable constructor
+			table = new HashMap<>();
+			for (int i = 0; i < recordCount; i++) {
+				table.put(i, new HashMap<>());
+			}
+		}
+
+		public boolean acquireLock(int recordId, int transactionId, LockType lockType) {
+			Map<Integer, LockType> locks = table.get(recordId);
+			if (locks.isEmpty()) { // no locks
+				locks.put(transactionId, lockType);
+				return true;
+			} else { // contains locks
+				if (locks.containsKey(transactionId)) { // current transaction has lock
+					LockType currentLockType = locks.get(transactionId);
+					if (lockType == LockType.SHARED) {
+						if (currentLockType == LockType.EXCLUSIVE) {
+							return true;
+						} else if (currentLockType == LockType.SHARED) {
+							return true;
+						}
+					} else if (lockType == LockType.EXCLUSIVE && currentLockType == LockType.EXCLUSIVE) { // current T already has EXCLUSIVE lock
+						return true;
+					}
+				} else { // current transaction don't have lock, so other transactions have locks
+					if (lockType == LockType.SHARED) {
+						if (!locks.containsValue(LockType.EXCLUSIVE)) {
+							locks.put(transactionId, lockType);
+							return true;
+						}
+					} else if (lockType == LockType.EXCLUSIVE) { // cannot obtain lock because other locks on other Ts
+						return false;
+					}
+				}
+			}
+			return false;
+		}
+
+		public void releaseLocks(int transactionId) {
+			for (Map<Integer, LockType> locks : table.values()) {
+				if (locks.containsKey(transactionId)) {
+					locks.remove(transactionId);
+				}
+			}
+		}
+
+		public boolean holdsLock(int recordId, int transactionId) {
+			Map<Integer, LockType> locks = table.get(recordId);
+			return locks.containsKey(transactionId);
+		}
+	}
+
+	public enum LockType {
+		NONE, SHARED, EXCLUSIVE
+	}
+
 
 	/**
 	 * Notes:
@@ -17,8 +84,7 @@ public class CC
 	 * @param transactions the schedule, which basically is a {@link List} of transactions.
 	 * @return the final status of the db
 	 */
-	public static int[] executeSchedule(int[] db, List<String> transactions)
-	{
+	public static int[] executeSchedule(int[] db, List<String> transactions) {
 		//TODO
 		return null ;
 	}
